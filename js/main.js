9a2af3cea0cf9297d238df3cdf909735f1497ece
Joseph Yhu <xbhlx7@gmail.com>
2020-03-28 14:51:03 -0400

    Move files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+new Vue({
+  el: '#app',
+  data: {
+    title: 'Full Stack Foundations Blog'
+  }
+});
+
+new Vue({
+  el: '#posts',
+  data: {
+      posts: '',
+      seen: true
+  },
+  mounted() {
+    fetch('http://localhost:8000/api/v1/posts')
+      .then(response => response.json())
+      .then(data => this.posts = data.posts)
+  }
+});
---
+++ js/post.js
@@ -0,0 +1,20 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
+
+new Vue({
+  el: '#post',
+  data: {
+    title: '',
+    timestamp: '',
+    content: ''
+  },
+  mounted() {
+    fetch(`http://localhost:8000/api/v1/posts/${id}`)
+      .then(response => response.json())
+      .then(data => {
+        this.title = data.title,
+        this.timestamp = data.timestamp,
+        this.content = data.content
+      })
+  }
+});
