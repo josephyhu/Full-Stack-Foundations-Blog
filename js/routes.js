a5cb567368791f0994732bc2799ef2a5e47ab98e
Joseph Yhu <xbhlx7@gmail.com>
2020-03-30 05:59:52 -0400

    Update files

--- js/routes.js
+++ js/routes.js
@@ -1,12 +1,28 @@
+function createPost(title, content) {
+  const path = 'http://localhost:8000/api/v1/posts';
+  axios.post(path, {title: title, content: content})
+    .then(response => response.data)
+    .then(() => window.location.replace('index.html'))
+    .then(() => alert('Post created'))
+}
+
+function create() {
+  title = document.querySelector('#title').value;
+  content = document.querySelector('#content').value;
+  createPost(title, content);
+}
+
 function updatePost(id, title, content) {
   const path = `http://localhost:8000/api/v1/posts/${id}`;
   axios.put(path, {title: title, content: content})
     .then(response => response.data)
     .then(() => window.location.replace(`post.html?id=${id}`))
-  alert('Post updated');
+    .then(() => alert('Post updated'))
 }
 
-function update(title, content) {
+function update() {
+  title = document.querySelector('#title').value;
+  content = document.querySelector('#content').value;
   updatePost(id, title, content);
 }
 
@@ -16,7 +32,7 @@ function deletePost(id) {
   axios.delete(path)
     .then(response => response.data)
     .then(() => window.location.replace('index.html'))
-  alert('Post deleted');
+    .then(() => alert('Post deleted'))
 }
 
 function del() {
